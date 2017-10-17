c01774ab504999705ebcd3df976e2a0e5910cd88
tuotuo65535 <562182858@qq.com>
2017-10-17 14:44:58 +0800

    add log4js

--- electron-quick-start/main.js
+++ electron-quick-start/main.js
@@ -1,6 +1,6 @@
 const electron = require('electron')
-//const log4js = require('log4js')
-//const log4js_config = require('./log4js.json')
+const log4js = require('log4js')
+const log4js_config = require('./log4js.json')
 const tracer = require('tracer')
 const fs = require('fs')
 // Module to control application life.
@@ -22,9 +22,9 @@ if(!logDirExt){
     fs.mkdirSync('./log');
 }
 
-//log4js.configure(log4js_config);
-//var logInfo = log4js.getLogger('logInfo');
-let logger = tracer.dailyfile({root:'./log', maxLogFiles: 2, allLogsFileName: 'tracer'})
+log4js.configure(log4js_config);
+var logger = log4js.getLogger('logInfo');
+//let logger = tracer.dailyfile({root:'./log', maxLogFiles: 2, allLogsFileName: 'tracer'})
 
 /**
  * 判断文件夹是否存在
@@ -57,15 +57,15 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    logger.debug('程序已关闭');
+    logger.info('程序已关闭');
     mainWindow = null
   })
 
   mainWindow.webContents.on('did-finish-load', () => {
-    logger.debug('程序已启动');
+    logger.error('程序已启动');
     mainWindow.webContents.send('main-process-messages', 'webContent event "did-finish-load" called');
       for(var i=1; i<1000; i++){
-          logger.debug('消息 "did-finish-load" 已发送');
+          logger.info('消息 "did-finish-load" 已发送');
       }
   });
 }
@@ -97,7 +97,7 @@ app.on('activate', function () {
 
 //async message
 ipcMain.on('asynchronous-message', (event, arg) => {
-    logger.debug('发送异步消息：ok');
+    logger.info('发送异步消息：ok');
     event.sender.send('asynchronous-reply', 'ok');
 })
 
