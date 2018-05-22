da764667e22f9fa4c2e6789e153a46575ee27d00
tuotuo65535 <562182858@qq.com>
2018-05-22 15:33:16 +0800

    搜索栏下滑逐渐不透明

--- mobile-dev/js/index.js
+++ mobile-dev/js/index.js
@@ -1,6 +1,7 @@
 (function(){
 	$(document).ready(() => {
 		clickBind();
+		searchScroll();
 	});
 
 	let clickBind = () => {
@@ -15,6 +16,22 @@
 			zdom.removeClass(className);
 		} else {
 			zdom.addClass(className);
+		}
+	}
+
+	//上滑隐藏显示搜素框
+	let searchScroll = () => {
+		let height = $('#intro-carousel').height();
+		window.onscroll = function() {
+			let top = document.body.scrollTop;
+			let opacity = 0;
+			if(top > height) {
+				opacity = 0.85;
+			} else {
+				opacity = 0.85 * (top/height);
+			}
+			console.log(opacity)
+			$('.navbar').css('background', 'rgba(0,0,0,' + opacity + ')');
 		}
 	}
 })();
